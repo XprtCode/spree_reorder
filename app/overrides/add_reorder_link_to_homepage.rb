1bf192bc9d6232b9e65168601760a063ad849e72
Praveen <spkprav@gmail.com>
2016-10-09 16:16:32 +0530

    deface original fix

--- app/overrides/add_reorder_link_to_homepage.rb
+++ app/overrides/add_reorder_link_to_homepage.rb
@@ -2,6 +2,7 @@ Deface::Override.new(
   :virtual_path  => "spree/shared/_main_nav_bar",
   :name => "reorder_link",
   :insert_after =>  "#home-link",
+  :original => '79d342a6fd7281d8499d3a5ee5480f416f733e98',
   :text => "
     <% unless @last_order.nil? %>
       <li><%= link_to Spree.t(:reorder), reorder_order_path(@last_order) %></li>
